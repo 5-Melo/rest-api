71eec49f40f0335c1ece3679df78b7e88b2afedb
AhmedMaged <osans026@gmail.com>
2025-01-26 20:21:40 +0200

    user CRUD OP (except for update and delete) && login request

--- src/main/java/com/MeloTech/user/UserController.java
+++ src/main/java/com/MeloTech/user/UserController.java
@@ -1,14 +1,53 @@
 package com.MeloTech.user;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("api/users")
 public class UserController {
     private final UserRepository userRepository;
 
-    UserController(UserRepository userRepository) {
+    public UserController(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
+
+    @GetMapping("")
+    private ArrayList<User> getAllUsers() {
+        return (ArrayList<User>) this.userRepository.findAll();
+    }
+
+    @GetMapping("{id}")
+    private ResponseEntity<Optional<User>> getUser(@PathVariable String id) {
+        Optional<User> user = this.userRepository.findById(id);
+
+        if (user.isPresent())
+            return new ResponseEntity<>(user, HttpStatus.OK);
+
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("")
+    private void addUser(@Valid @RequestBody User user) {
+        this.userRepository.save(user);
+    }
+
+    @PostMapping("/login")
+    private ResponseEntity<?> loginUser(@RequestBody User credentials) {
+        User user = this.userRepository.findByUsernameAndPassword(credentials.getUsername(), credentials.getPassword());
+
+        if (user == null)
+            return new ResponseEntity<>("invalid username or password", HttpStatus.UNAUTHORIZED);
+
+        return ResponseEntity.ok(user);
+    }
+
+
 }
