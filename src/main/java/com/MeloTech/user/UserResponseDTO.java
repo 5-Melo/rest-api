a007c90934f5d46504395a8ef2c5a58a4b04e1cf
AhmedMaged <osans026@gmail.com>
2025-02-08 08:51:25 +0200

    feat: replaced the complete user object with userDTO in response body for better modularity and security concerns

---
+++ src/main/java/com/MeloTech/user/UserResponseDTO.java
@@ -0,0 +1,57 @@
+package com.MeloTech.user;
+
+// user's specific attributes returned in the response body (improves security and maintainability)
+public class UserResponseDTO {
+    private String id;
+    private String firstName;
+    private String lastName;
+    private String username;
+    private String email;
+    public UserResponseDTO(String id, String firstName, String lastName, String username, String email) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.username = username;
+        this.email = email;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
